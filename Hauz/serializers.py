94b185125e471d6ca39959a006b4497ad01a9339
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-13 12:59:05 +0300

    Create a UserSerializer
    * Define the attributes
    * Create method that creates user object
    
    * Define the User model as corresponding model for the UserSerializer

---
+++ Hauz/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True, validators=[
+                                   UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())])
+    first_name = serializers.CharField(max_length=30)
+    last_name = serializers.CharField(max_length=30)
+    password = serializers.CharField(min_length=8)
+
+    # create new usser method using create_user method from the User Class
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['email'], validated_data['first_name'],
+                                        validated_data['last_name'], validated_data['username'], validated_data['password'])
+        return user
+
+    class Meta:
+        model = User
+        fields = ('id', 'first_name'. 'last_name', 'email', 'password')
