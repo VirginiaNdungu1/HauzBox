ca391d80545e76c5ca3c3fd1c9eff1736a5ba889
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-14 18:58:21 +0300

    Create API View - CreateUser
    * create post function to create a new user

--- Hauz/views.py
+++ Hauz/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . serializers import UserSerializer
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+class CreateUser(APIView):
+    '''
+    creates user
+    '''
+
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
