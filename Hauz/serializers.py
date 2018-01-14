5ec424348d2a7d8d9e47caa3dbedd67629da98de
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-14 18:59:00 +0300

    Update the username ( max_length=32)

--- Hauz/serializers.py
+++ Hauz/serializers.py
@@ -6,18 +6,16 @@ from rest_framework.validators import UniqueValidator
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True, validators=[
                                    UniqueValidator(queryset=User.objects.all())])
-    username = serializers.CharField(
-        validators=[UniqueValidator(queryset=User.objects.all())])
-    # first_name = serializers.CharField(max_length=30)
-    # last_name = serializers.CharField(max_length=30)
-    password = serializers.CharField(min_length=8)
+    username = serializers.CharField(max_length=32,
+                                     validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(min_length=8, write_only=True)
 
     # create new usser method using create_user method from the User Class
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'], validated_data['first_name'],
-                                        validated_data['last_name'], validated_data['username'], validated_data['password'])
+        user = User.objects.create_user(
+            validated_data['username'], validated_data['email'], validated_data['password'])
         return user
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'password')
+        fields = ('id', 'username', 'email', 'password')
