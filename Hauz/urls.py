31cff0bb4736ba659cabd77622005e96cb6e4ed8
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-14 18:57:14 +0300

    Update - configure create new user urls

--- Hauz/urls.py
+++ Hauz/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'api/users^$', views.CreateUser.as_view(), name='account-create'),
+    url(r'^api/users/$', views.CreateUser.as_view(), name='account-create'),
 ]
