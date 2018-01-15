da80565d8e0e7bf93a2a316d0e34370acff478a6
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-15 12:33:51 +0300

    Update CreateUser APIView
    * generate token on user-registration

--- Hauz/views.py
+++ Hauz/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . serializers import UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 
@@ -17,5 +21,8 @@ class CreateUser(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
