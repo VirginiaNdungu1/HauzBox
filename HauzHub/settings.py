fecd3555acf4999b2095c1d2fee309e62de2d271
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-15 12:44:41 +0300

    django-cors-headers for handing server headers
    * Add corsheaders to INSTALLED_APPS
    
    * Add a middleware class to listen in on responses.
    
    * Configure middleware's behaviour by seetting it to True

--- HauzHub/settings.py
+++ HauzHub/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'bootstrap3',
     'registration',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -144,3 +146,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
