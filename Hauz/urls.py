496967d6012b9c7a3ac55ea6ea08d9522b5befe6
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-15 12:34:30 +0300

    Update CreateUser APIView endpoint

--- Hauz/urls.py
+++ Hauz/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^api/users/$', views.CreateUser.as_view(), name='account-create'),
+    url(r'^api/users/register/$', views.CreateUser.as_view(), name='account-create'),
 ]
