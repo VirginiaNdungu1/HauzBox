82575530c6404a16505012a5136d8764a83ad786
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-17 14:46:57 +0300

    Register Model classes *Property * House * Maintenance * Amenity * Tenant * Payment * Expense Create Custom ModelAdmin Classes * HouseAdmin * ExpenseAdmin

--- Hauz/admin.py
+++ Hauz/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from . models import Property_Type
+from . models import Property_Type, Property, House, Maintenance, Amenity, Tenant, Payment, Expense
+# Create a custom ModelAdmin class
+
+
+class HouseAdmin(admin.ModelAdmin):
+    filter_horizontal = ('amenity_id')
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    filter_horizontal = ('maintenance')
+
+
 # Register your models here.
 admin.site.register(Property_Type)
+admin.site.register(Property)
+admin.site.register(House, HouseAdmin)
+admin.site.register(Amenity)
+admin.site.register(Tenant)
+admin.site.register(Maintenance, ExpenseAdmin)
+admin.site.register(Expense)
+admin.site.register(Payment)
