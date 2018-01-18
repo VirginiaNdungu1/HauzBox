bd1856b324e41e16c627c4f7153ab11db1c40555
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-18 16:20:46 +0300

    Configure DRF's Default Permission Classes

--- HauzHub/settings.py
+++ HauzHub/settings.py
@@ -151,7 +151,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
