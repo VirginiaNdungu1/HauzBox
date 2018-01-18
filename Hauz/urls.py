77d322a28975f1fffca701a7dd926eaf747e5405
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-18 16:28:42 +0300

    Configure PropertyGroupList Api View

--- Hauz/urls.py
+++ Hauz/urls.py
@@ -5,4 +5,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^api/users/register/$', views.CreateUser.as_view(), name='account-create'),
+    url(r'^api/property_group/(?P<user_id>[0-9]+)/$',
+        views.PropertyGroupList.as_view()),
 ]
