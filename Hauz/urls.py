f01050f79721286fb8904cc75e1fd1ea5ec1fa6d
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-19 07:47:06 +0300

    Configure Property Types

--- Hauz/urls.py
+++ Hauz/urls.py
@@ -5,6 +5,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^api/users/register/$', views.CreateUser.as_view(), name='account-create'),
-    url(r'^api/property_group/(?P<user_id>[0-9]+)/$',
+    url(r'^api/property_group/$',
         views.PropertyGroupList.as_view()),
+    url(r'^api/property_types/$', views.PropertyTypeList.as_view()),
+    # url(r'^api/property_group/properties/(?P<property_group_id>[0-9]+)/$',
+    #     views.PropertiesList.as_view()),
 ]
