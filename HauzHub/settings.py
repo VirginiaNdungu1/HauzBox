dd55f5608fc7159f493577623fb969d071f6bdad
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-19 11:32:34 +0300

    Add extra permission classes

--- HauzHub/settings.py
+++ HauzHub/settings.py
@@ -154,6 +154,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
 }
 
