8659072eb679f40a7403206f1e7d601a68d4f0a6
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-20 18:59:10 +0300

    Minor Changes

--- Hauz/models.py
+++ Hauz/models.py
@@ -28,28 +28,6 @@ class Property_Group(models.Model):
         return self.name
 
 
-class Property(models.Model):
-    class Meta:
-        db_table = 'property'
-
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=250, blank=True)
-    house_count = models.PositiveIntegerField(default=0)
-    property_type = models.ForeignKey(
-        Property_Type, on_delete=models.CASCADE)
-    property_group = models.ForeignKey(
-        Property_Group, on_delete=models.CASCADE, related_name='group_property', null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='user_property')
-
-    # def __unicode__(self):
-    #     return '%d: %s' % (self.id, self.name)
-
-    def __str__(self):
-        return self.name
-
-
 class Amenity(models.Model):
     class Meta:
         db_table = 'amenity'
@@ -58,6 +36,26 @@ class Amenity(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='property_amenity')
+
+    def __str__(self):
+        return self.name
+
+
+class Property(models.Model):
+    class Meta:
+        db_table = 'property'
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=250, blank=True)
+    house_count = models.PositiveIntegerField(default=0)
+    property_type = models.ForeignKey(
+        Property_Type, on_delete=models.CASCADE, related_name='all_property_types')
+    property_group = models.ForeignKey(
+        Property_Group, on_delete=models.CASCADE, related_name='group_property', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_property')
+    amenities = models.ManyToManyField(Amenity)
 
     def __str__(self):
         return self.name
@@ -90,12 +88,11 @@ class House(models.Model):
     price = models.DecimalField(max_digits=14, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     occupancy = models.BooleanField(default=False)
-    property_id = models.ForeignKey(
+    property_types = models.ForeignKey(
         Property_Type, on_delete=models.CASCADE, related_name='house_id')
     house_property = models.ForeignKey(
         Property, on_delete=models.CASCADE, related_name='property_houses', null=True)
-    amenity_id = models.ManyToManyField(Amenity)
-    tenant_id = models.ForeignKey(
+    tenants = models.ForeignKey(
         Tenant, on_delete=models.CASCADE, related_name='house_tenant', blank=True, null=True)
 
     def __str__(self):
@@ -140,7 +137,7 @@ class Expense(models.Model):
     gabbage = models.DecimalField(max_digits=10, decimal_places=2)
     security = models.DecimalField(max_digits=10, decimal_places=2)
     cleaning = models.DecimalField(max_digits=10, decimal_places=2)
-    maintenance_id = models.ManyToManyField(Maintenance)
+    maintenances = models.ManyToManyField(Maintenance)
     property_tax = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
