f390923788c7d800cef18c36680dceaca360e3f1
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-22 19:34:48 +0300

    Minor changes
    * Add month property to Payment
    * str(house id)

--- Hauz/models.py
+++ Hauz/models.py
@@ -121,13 +121,15 @@ class Payment(models.Model):
     house = models.ForeignKey(
         House, on_delete=models.CASCADE, related_name='house_payment')
     transaction_id = models.CharField(max_length=50)
-    paid_at = models.TimeField(
-        auto_now=False, auto_now_add=False, null=True)
+    month = models.CharField(
+        max_length=30, choices=Month_Choices, default='jan', blank=True)
+    # paid_at = models.TimeField(
+    #     auto_now=False, auto_now_add=False, null=True)
     amount = models.DecimalField(max_digits=14, decimal_places=2)
     is_paid = models. BooleanField(default=False)
 
     def __str__(self):
-        return self.transaction_id
+        return str(self.house)
 
 
 class Maintenance(models.Model):
@@ -157,4 +159,4 @@ class Expense(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return self.id
+        return self.month
