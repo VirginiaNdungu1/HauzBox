bf84a75aa3f17d67d20b50d89e402ae327da7d4b
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-23 08:01:58 +0300

    Configure PropertyExpenseList view function

--- Hauz/urls.py
+++ Hauz/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^api/property_group/$',
         views.PropertyGroupList.as_view()),
     url(r'^api/property_types/$', views.PropertyTypeList.as_view()),
+    url(r'^api/properties/$', views.PropertyExpenseList.as_view()),
     # url(r'^api/property_group/properties/(?P<property_group_id>[0-9]+)/$',
     #     views.PropertiesList.as_view()),
 ]
