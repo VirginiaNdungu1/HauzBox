262344d787fd4a30961fc257d7f20a263abde430
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-24 16:48:46 +0300

    Configure url - PropertiesList APIView

--- Hauz/urls.py
+++ Hauz/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     url(r'^api/property_group/$',
         views.PropertyGroupList.as_view()),
     url(r'^api/property_types/$', views.PropertyTypeList.as_view()),
-    url(r'^api/properties/$', views.PropertyExpenseList.as_view()),
-    url(r'^api/properties/new/$', views.PropertyList.as_view()),
+    url(r'^api/properties/$', views.PropertiesList.as_view()),
+    # url(r'^api/properties/new/$', views.PropertyList.as_view()),
     url(r'^api/payment/new/$', views.Payments.as_view()),
-    
+
     # url(r'^api/property_group/properties/(?P<property_group_id>[0-9]+)/$',
     #     views.PropertiesList.as_view()),
 ]
