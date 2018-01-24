1624123790f4b877f5ea463962069f71001a47de
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2018-01-24 16:50:15 +0300

    forms.py

--- Hauz/forms.py
+++ Hauz/forms.py
@@ -1,7 +1,9 @@
-from .models import *
-from django.forms import ModelForm
-
-class PaymentForm(ModelForm):
-    class Meta:
-        model = Payment
-        fields = ['tenant_name', 'month', 'transaction_id', 'amount', 'property_id']
+# from .models import *
+# from django.forms import ModelForm
+#
+#
+# class PaymentForm(ModelForm):
+#     class Meta:
+#         model = Payment
+#         fields = ['tenant_name', 'month',
+#                   'transaction_id', 'amount', 'property_id']
